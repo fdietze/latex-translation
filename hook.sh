4dc79c0a2322cebe3cf6bc07e354c75655ec2133
Felix Dietze <felix@beerleader.net>
2012-12-22 21:24:36 +0100

    git clean and reset output in log

--- hook.sh
+++ hook.sh
@@ -4,9 +4,9 @@ REPO=../rulebook-latex
 POOTLE=../pootle
 
 cd $REPO
-  git reset --hard
-  git clean -df
-  git pull 2>&1 | tee $REPO/out/make.log
+  git reset --hard 2>&1 | tee $REPO/out/make.log
+  git clean -df 2>&1 | tee -a $REPO/out/make.log
+  git pull 2>&1 | tee -a $REPO/out/make.log
 cd -
 
 make REPO=$REPO POOTLE=$POOTLE 2>&1 | tee -a $REPO/out/make.log
