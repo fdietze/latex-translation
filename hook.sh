f5a354f4b20da5c5a05e23c40ceff1184f6e7478
Felix Dietze <felix@beerleader.net>
2012-12-22 21:22:18 +0100

    add git reset, put git errors in log file

--- hook.sh
+++ hook.sh
@@ -4,8 +4,9 @@ REPO=../rulebook-latex
 POOTLE=../pootle
 
 cd $REPO
+  git reset --hard
   git clean -df
-  git pull 2>&1
+  git pull 2>&1 | tee $REPO/out/make.log
 cd -
 
-make REPO=$REPO POOTLE=$POOTLE 2>&1 | tee $REPO/out/make.log
+make REPO=$REPO POOTLE=$POOTLE 2>&1 | tee -a $REPO/out/make.log
