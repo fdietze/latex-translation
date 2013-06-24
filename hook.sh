8a2de27f942c81cbdb3c52b551e774349db27542
Felix Dietze <felix@beerleader.net>
2013-06-24 13:30:43 +0100

    execute make inside repo directory, use local latex commands

--- hook.sh
+++ hook.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# TODO: integrate pdfopt
+
+PATH=/usr/local/texlive/2013/bin/x86_64-linux:$PATH
+
 REPO=../rulebook-latex
 POOTLE=../pootle
+LOGFILE=$REPO/out/make.log
+PWD=’pwd’
+
 
 cd $REPO
-  git reset --hard 2>&1 | tee $REPO/out/make.log
-  git clean -df 2>&1 | tee -a $REPO/out/make.log
-  git pull 2>&1 | tee -a $REPO/out/make.log
-cd -
 
-make REPO=$REPO POOTLE=$POOTLE 2>&1 | tee -a $REPO/out/make.log
+git reset --hard 2>&1 | tee $LOGFILE
+git clean -df 2>&1 | tee -a $LOGFILE
+git checkout master 2>&1 | tee -a $LOGFILE #triggers checkout hook
+git pull --rebase 2>&1 | tee -a $LOGFILE #rebase, to track history changes
+
+make --makefile=$PWD/Makefile REPO=$REPO POOTLE=$POOTLE 2>&1 | tee -a $LOGFILE
